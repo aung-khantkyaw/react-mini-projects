2373333289a59f66fce99b2c61b384dd02a894dd
aung-khantkyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2024-04-21 22:03:04 +0630

    Generate Profile Card

---
+++ generate-profile-card/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Button} from 'antd'
+import InfoForm from './InfoForm'
+ 
+function App() {
+
+  return (
+    <>
+      <InfoForm/>
+    </>
+  )
+}
+
+export default App
